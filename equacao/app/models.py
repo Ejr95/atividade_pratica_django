eb30c4440d52ef824cb5fd2b41b186c775d17470
Elizeu Magalhães <Elizeu.2230556@aluno.faculdadedescomplica.com.br>
2022-11-22 14:24:18 -0400

    implementacao do model e configuraçoes de rotas

--- equacao/app/models.py
+++ equacao/app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    
+    a = models.IntegerField("A")
+    
+    b = models.IntegerField("B")
+    
+    c = models.IntegerField("C")
+    raiz1=0
+    raiz2=0
+    
+    def __str__(self):
+        return f"Coeficiente A: {self.a}, Coeficiente B: {self.b}, Coeficiente C: {self.c}"
--- equacao/app/views.py
+++ equacao/app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import UserForm
 
-# Create your views here.
+def index(request):
+    if request.method == 'GET':
+        form = UserForm()
+        context = {
+            'form' : form
+        }
+        return render(request, 'user/index.html', context=context)
+    else:
+        form = UserForm(request.POST)
+        if form.is_valid():
+
+            a =  form.cleaned_data['a'],
+            b = form.cleaned_data['b'],
+            c = form.cleaned_data['c']
+
+            delta = (b ** 2) - 4*(a*c)
+
+            raiz1 = (-b + (0.5**delta))/(2*a)
+            raiz2 = (-b - (0.5**delta))/(2*a)
+
+            context = {
+                'form' : form,
+                'raiz1': raiz1,
+                'raiz2': raiz2,
+                'delta':delta,
+            }
+        return render(request, 'user/index.html', context=context)
